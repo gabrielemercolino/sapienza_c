aa86ea8922df76a0564bbc792d105f44ce555ef5
Gabriele <gmercolino2003@gmail.com>
2025-03-04 10:03:08 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "c dev flake";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [gcc gnumake];
+          shellHook = ''
+            echo "C env activated"
+          '';
+        };
+      }
+    );
+}
